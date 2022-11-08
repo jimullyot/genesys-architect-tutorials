4d12b4b5d22ba5d950f795a56527964025555d47
Jim Ullyot <jim.ullyot@genesys.com>
2022-11-08 10:14:55 -0500

    getting some Genesys naming in place

--- tutorials/module_json/config.tf
+++ tutorials/module_json/config.tf
@@ -32,33 +32,29 @@ provider "genesyscloud" {
   aws_region         = var.aws_region
 }
 
-locals {
-  studentFlowName = join("", ["Tutorial-", var.studentFirstName, "-JSON-Demo-01"])
-}
-
-resource "genesyscloud_flow" "Tutorial-JSON-Demo-01" {
-  filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_json/flows/Tutorial-JSON-Demo-01.yaml"
+resource "genesyscloud_flow" "Genesys-Tutorial-JSON-Demo-01" {
+  filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_json/flows/Genesys-Tutorial-JSON-Demo-01.yaml"
   file_content_hash = "b256d5d19e3977f411b924d738f453b6d0aca88d"
   substitutions = {
-    flow_name          =  join("", ["Tutorial-", var.studentFirstName, "-JSON-Demo-01"])
-    student_first_name = local.studentFlowName
+    flow_name          =  join("", ["Genesys-Tutorial-", var.studentFirstName, "-JSON-Demo-01"])
+    student_first_name = var.studentFirstName
   }
 }
 
-resource "genesyscloud_flow" "Tutorial-JSON-Demo-02" {
-  filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_json/flows/Tutorial-JSON-Demo-02.yaml"
+resource "genesyscloud_flow" "Genesys-Tutorial-JSON-Demo-02" {
+  filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_json/flows/Genesys-Tutorial-JSON-Demo-02.yaml"
   file_content_hash = "a35b890f32b02d4a33857af1865a54d32c1c776b"
   substitutions = {
-    flow_name          =  join("", ["Tutorial-", var.studentFirstName, "-JSON-Demo-02"])
-    student_first_name = local.studentFlowName
+    flow_name          =  join("", ["Genesys-Tutorial-", var.studentFirstName, "-JSON-Demo-02"])
+    student_first_name = var.studentFirstName
   }
 }
 
-resource "genesyscloud_flow" "Tutorial-JSON-Demo-03" {
-  filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_json/flows/Tutorial-JSON-Demo-03.yaml"
+resource "genesyscloud_flow" "Genesys-Tutorial-JSON-Demo-03" {
+  filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_json/flows/Genesys-Tutorial-JSON-Demo-03.yaml"
   file_content_hash = "a35b890f32b02d4a33857af1865a54d32c1c776b"
   substitutions = {
-    flow_name          =  join("", ["Tutorial-", var.studentFirstName, "-JSON-Demo-03"])
-    student_first_name = local.studentFlowName
+    flow_name          =  join("", ["Genesys-Tutorial-", var.studentFirstName, "-JSON-Demo-03"])
+    student_first_name = var.studentFirstName
   }
 }
