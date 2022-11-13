d337456c86cc973157c082cd87fca95ab4696053
Jim Ullyot <jim.ullyot@genesys.com>
2022-11-13 10:16:31 -0500

    More tweaks for ssml initial support

--- config.tf
+++ config.tf
@@ -24,9 +24,18 @@ variable "aws_region" {
   default = "dca"
 }
 
-module "tutorial_json" {
+#module "tutorial_json" {
+#  # count             = tutorials == "module_json" || tutorials == "all") ? 1 : 0
+#  source            = "./tutorials/module_json"
+#  aws_region        = var.aws_region
+#  oauthClientId     = var.oauthClientId
+#  oauthClientSecret = var.oauthClientSecret
+#  studentFirstName  = var.studentFirstName
+#}
+
+module "tutorial_ssml" {
   # count             = tutorials == "module_json" || tutorials == "all") ? 1 : 0
-  source            = "./tutorials/module_json"
+  source            = "./tutorials/module_ssml"
   aws_region        = var.aws_region
   oauthClientId     = var.oauthClientId
   oauthClientSecret = var.oauthClientSecret
--- tutorials/module_ssml/config.tf
+++ tutorials/module_ssml/config.tf
@@ -32,22 +32,21 @@ provider "genesyscloud" {
   aws_region         = var.aws_region
 }
 
-Genesys-Tutorial-Demo-SSML-Bot-01
-
-resource "genesyscloud_flow" "Genesys-Tutorial-SSMO-Demo-Bot-01" {
+resource "genesyscloud_flow" "Genesys-Tutorial-SSML-Demo-Bot-01" {
   filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_ssml/flows/Genesys-Tutorial-SSML-Demo-Bot-01.yaml"
-  file_content_hash = "b256d5d19e3977f411b924d738f453b6d0aca88d"
+  file_content_hash = "53ee7551b71b020bcc62ae54ebbe008874291412"
   substitutions = {
-    flow_name          =  join("", ["Genesys-Tutorial-", var.studentFirstName, "-JSON-Demo-01"])
+    flow_name          = join("", ["Genesys-Tutorial-", var.studentFirstName, "-SSML-Demo-Bot-01"])
     student_first_name = var.studentFirstName
   }
 }
 
-resource "genesyscloud_flow" "Genesys-Tutorial-JSON-Demo-02" {
+resource "genesyscloud_flow" "Genesys-Tutorial-SSML-Demo-01" {
   filepath = "https://raw.githubusercontent.com/jimullyot/genesys-architect-tutorials/main/tutorials/module_ssml/flows/Genesys-Tutorial-SSML-Demo-01.yaml"
-  file_content_hash = "a35b890f32b02d4a33857af1865a54d32c1c776b"
+  file_content_hash = "ae7812cadb7c7593a3fb2809792a9f338e870ca4"
   substitutions = {
-    flow_name          =  join("", ["Genesys-Tutorial-", var.studentFirstName, "-JSON-Demo-02"])
-    student_first_name = var.studentFirstName
+    flow_name                   = join("", ["Genesys-Tutorial-", var.studentFirstName, "-SSML-Demo-01"])
+    ssml_tutorial_bot_flow_name = join("", ["Genesys-Tutorial-", var.studentFirstName, "-SSML-Demo-Bot-01"])
+    student_first_name          = var.studentFirstName
   }
 }
